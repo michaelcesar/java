985ecb8eed4e18042b7e5925da1805a13ff5e7e0
michaelcesar <michaelcesarflopesgit@gmail.com>
2024-11-12 20:38:35 -0300

    run

--- Main.java
+++ Main.java
@@ -4,16 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car myCar = new Car();
-
+        Car myCar = new Car("Fusca");
+        myCar.run();
 
     }
 }
 
 
 class Car {
+    String model;
 
-    public Car(){
-        System.out.println("Car is created");
+    public Car(String model){
+        this.model = model;
+    }
+
+    public void run(){
+        System.out.println("Car is running");
     }
 }
